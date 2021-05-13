1e7a0f656e94324f888630c002a5683de9b243c5
Maxence Lecanu <lecanu.maxence@gmail.com>
2021-05-13 23:33:06 +0200

    feat(staged-files): add configuration for when hooks change files

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -1,4 +1,10 @@
+export enum ADDED_BEHAVIORS {
+  ADD_AND_COMMIT = 'addAndCommit',
+  EXIT = 'exit',
+}
+
 export interface Config {
   packagesPath: string;
   packages: string[];
+  addedBehavior: ADDED_BEHAVIORS;
 }
