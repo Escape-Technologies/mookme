d10c0e5b622b112a2ddbc809fd7d9e0c0242262b
Maxence Lecanu <lecanu.maxence@gmail.com>
2021-05-21 16:01:26 +0200

    feat(documentation): auto-publish documentation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,9 @@ module.exports = {
   title: "",
   description:
     "The documentation for the Escape command-line tools and applications",
-  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   smoothScroll: true,
-  patterns: ["**/*.md", "**/*.vue", "!instrumenters/python/**/*.md"],
+  patterns: ["**/*.md", "**/*.vue"],
   themeConfig: {
     logo: "/logo.png",
     displayAllHeaders: true,
