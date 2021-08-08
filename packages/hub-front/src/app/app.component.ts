f1c0e9b4faa0e7940918d02967f8c4d30265f73d
Maxence Lecanu <lecanu.maxence@gmail.com>
2021-08-08 16:33:57 +0200

    feat(mookme-hub): add frontend scaffold

---
+++ packages/hub-front/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+}
