d47526b98fe0bafeaf50f13fc65bfdd6453cd6b3
Maxence Lecanu <lecanu.maxence@gmail.com>
2021-08-20 23:43:49 +0200

    fix(mookme): reordered UI module

--- packages/mookme/src/utils/hook-package.ts
+++ packages/mookme/src/utils/hook-package.ts
@@ -2,29 +2,7 @@ import chalk from 'chalk';
 import { PackageHook } from '../types/hook.types';
 import { runStep } from './run-step';
 import { StepCommand, StepError } from '../types/step.types';
-import { spin, SpinnerManager } from './spinner';
-
-interface UI {
-  packageLogger: (log: string) => void;
-  stepsSpinners: { [key: string]: SpinnerManager };
-}
-
-function init_ui(hook: PackageHook): UI {
-  const ui: UI = {
-    packageLogger: console.draft(
-      `${chalk.bold.inverse(` Hooks : ${hook.name} `)}${chalk.bgBlueBright.bold(' Running... ')}`,
-    ),
-    stepsSpinners: {},
-  };
-  console.log();
-
-  for (const step of hook.steps) {
-    console.log(`→ ${chalk.bold(step.name)} > ${step.command} `);
-    ui.stepsSpinners[step.name] = spin('Scheduled');
-  }
-
-  return ui;
-}
+import { init_ui, UI } from './ui';
 
 function handleStepError(
   stepError: {
--- packages/mookme/src/utils/ui.ts
+++ packages/mookme/src/utils/ui.ts
@@ -1,4 +1,28 @@
 import chalk from 'chalk';
+import { PackageHook } from '../types/hook.types';
+import { spin, SpinnerManager } from './spinner';
+
+export interface UI {
+  packageLogger: (log: string) => void;
+  stepsSpinners: { [key: string]: SpinnerManager };
+}
+
+export function init_ui(hook: PackageHook): UI {
+  const ui: UI = {
+    packageLogger: console.draft(
+      `${chalk.bold.inverse(` Hooks : ${hook.name} `)}${chalk.bgBlueBright.bold(' Running... ')}`,
+    ),
+    stepsSpinners: {},
+  };
+  console.log();
+
+  for (const step of hook.steps) {
+    console.log(`→ ${chalk.bold(step.name)} > ${step.command} `);
+    ui.stepsSpinners[step.name] = spin('Scheduled');
+  }
+
+  return ui;
+}
 
 export const center = (txt: string, sep = '-'): void => {
   console.log(
