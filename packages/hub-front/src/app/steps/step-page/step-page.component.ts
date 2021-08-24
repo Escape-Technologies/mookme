1ceee32cfa042b834c415aa9c3079883bf4b9af6
Maxence Lecanu <lecanu.maxence@gmail.com>
2021-08-24 09:50:08 +0200

    fix(hub-front): remove angular starter

--- packages/hub-front/src/app/steps/step-page/step-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-step-page',
-  templateUrl: './step-page.component.html',
-  styleUrls: ['./step-page.component.scss'],
-})
-export class StepPageComponent {}
