7a8078cb18aecdf52c7e6939885e0c1be1e2f0a8
Maxence Lecanu <lecanu.maxence@gmail.com>
2022-03-31 22:33:46 +0200

    fix(mookme): fix post-commit hook with previous commit strategy

--- packages/mookme/src/loaders/filter-strategies/previous-commit-filter.ts
+++ packages/mookme/src/loaders/filter-strategies/previous-commit-filter.ts
@@ -1,11 +1,21 @@
-import { PackageHook } from '../../types/hook.types';
+import { GitToolkit } from '../../utils/git';
 import { FilterStrategy } from './base-filter';
 
 /**
  * A base class for denoting a strategy used to filter hooks to run
  */
 export class PreviousCommitFilterStrategy extends FilterStrategy {
-  async filter(hooks: PackageHook[]): Promise<PackageHook[]> {
-    return hooks;
+  useAllFiles: boolean;
+
+  /**
+   * @param gitToolkit - the {@link GitToolkit} instance to use to manage the VCS state
+   */
+  constructor(gitToolkit: GitToolkit, useAllFiles = false) {
+    super(gitToolkit);
+    this.useAllFiles = useAllFiles;
+  }
+
+  geFilesPathList(): string[] {
+    return this.useAllFiles ? this.gitToolkit.getAllTrackedFiles() : this.gitToolkit.getPreviouslyCommitedFiles();
   }
 }
